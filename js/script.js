c33ecb387fd567aae1fd3ac6cc52e608afbe6274
luis-hdz7 <luisjorgeantonhernandez@gmail.com>
2026-05-02 09:32:03 -0600

    Arreglando el localStorage del dark mode de la pagina

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 const btn = document.querySelector(".theme")
 const icon = btn.querySelector(".changeTheme")
-btn.addEventListener("click",()=>{
+let darkMode=localStorage.getItem("darkMode")
+const habilitarDarkMode=()=>{
+    document.body.classList.add("darkMode")
+    localStorage.setItem("darkMode","habilitado")
     document.body.classList.toggle('dark-mode')
     icon.classList.toggle("fa-sun")
     icon.classList.toggle("fa-moon")
-})
+}
+const deshabilitarDarkMode=()=>{
+    document.body.classList.remove("darkMode")
+    localStorage.setItem("darkMode","deshabilitado")
+    document.body.classList.toggle('dark-mode')
+    icon.classList.toggle("fa-sun")
+    icon.classList.toggle("fa-moon")
+}
+if (darkMode==="habilitado"){
+    habilitarDarkMode()
+}
+btn.addEventListener("click", () => {
+    const modoOscuroActual = localStorage.getItem("darkMode");
+    if (modoOscuroActual !== "habilitado") {
+        habilitarDarkMode();
+    } else {
+        deshabilitarDarkMode();
+    }
+});
